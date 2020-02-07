f695273a8a17e7d3674b3fb0adb837594e38a918
naicsss <56775102+naicsss@users.noreply.github.com>
2020-02-07 16:45:15 -0300

    react app creada y bulma importada

---
+++ appmovie/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Title } from './Components/Title';
+
+import './App.css';
+import 'bulma/css/bulma.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Search Movies
+      </div>
+    );
+  }
+}
+
+export default App;
