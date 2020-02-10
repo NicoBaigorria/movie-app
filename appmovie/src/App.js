b315077be321dd8c2bfce78c977fd979fe4a3411
naicsss <56775102+naicsss@users.noreply.github.com>
2020-02-10 02:03:14 -0300

    cards ready

--- appmovie/src/App.js
+++ appmovie/src/App.js
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
 import { Title } from './Components/Title';
 import { SearchForm } from './Components/SearchForm';
+import { MovieList } from './Components/MovieList';
 
 import './App.css';
 import 'bulma/css/bulma.css';
 
 class App extends Component {
-  state = { results: [] }
+  state = { usedSearch: false, results: [] }
 
   _handleResults = (results) => {
-    this.setState({results})
+    this.setState({results, usedSearch: true })
   }
 
   _renderResults () {
-    const { results } = this.state
-    return results.map(movie => {
-      return <p key={movie.imdbID}>{movie.Title}</p>
-    })
+    return this.state.results.length === 0
+      ? <p> Perdon! Resultado no encontrado! </p>
+      : <MovieList movies={this.state.results} />
   }
 
   render() {
     return (
       <div className="App">
-        <Title>Search Movies</Title>
+        <Title>Buscar Pelicula</Title>
         <div className='SearchForm-wrapper'>
           <SearchForm onResults={this._handleResults} />
         </div>
-        {this.state.results.length === 0
-          ? <p> Sin resultados </p>
-          : this._renderResults()
+        {this.state.usedSearch
+          ? this._renderResults()
+          : <small> Usa el formulario para buscar una pelicula </small>
         }
       </div>
     );
